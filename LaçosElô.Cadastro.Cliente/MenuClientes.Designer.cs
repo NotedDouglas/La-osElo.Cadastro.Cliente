f3342ddbb498d0a2a7a5199393a3a2f062d12865
Douglas Pimentel <158001654+NotedDouglas@users.noreply.github.com>
2026-03-26 20:52:28 -0300

    Refatoração do DataGridView: aplicação de cores conforme status (Ativo/Cancelado) e exibição de fotos com fallback padrão

--- LaçosElô.Cadastro.Cliente/MenuClientes.Designer.cs
+++ LaçosElô.Cadastro.Cliente/MenuClientes.Designer.cs
@@ -24,6 +24,21 @@
         /// </summary>
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuClientes));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btCadastro = new System.Windows.Forms.Button();
             this.btPdf = new System.Windows.Forms.Button();
@@ -31,6 +46,24 @@
             this.btAdd = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.dgLista = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.foto = new System.Windows.Forms.DataGridViewImageColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nasc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.documento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.celular = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.endereco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estado_civil = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cep = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bairro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.obs = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.situacao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.estado = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgLista)).BeginInit();
             this.SuspendLayout();
@@ -109,13 +142,230 @@
             // 
             // dgLista
             // 
+            this.dgLista.AllowUserToAddRows = false;
+            this.dgLista.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightCyan;
+            this.dgLista.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.foto,
+            this.nome,
+            this.nasc,
+            this.documento,
+            this.celular,
+            this.endereco,
+            this.email,
+            this.genero,
+            this.rg,
+            this.estado_civil,
+            this.cep,
+            this.numero,
+            this.bairro,
+            this.cidade,
+            this.obs,
+            this.situacao,
+            this.estado});
             this.dgLista.Location = new System.Drawing.Point(12, 133);
             this.dgLista.Name = "dgLista";
-            this.dgLista.RowHeadersWidth = 51;
-            this.dgLista.RowTemplate.Height = 24;
-            this.dgLista.Size = new System.Drawing.Size(1398, 528);
+            this.dgLista.ReadOnly = true;
+            this.dgLista.RowHeadersVisible = false;
+            this.dgLista.RowHeadersWidth = 4;
+            this.dgLista.RowTemplate.Height = 30;
+            this.dgLista.Size = new System.Drawing.Size(1494, 528);
             this.dgLista.TabIndex = 1;
+            this.dgLista.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgLista_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "N°";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 40;
+            // 
+            // foto
+            // 
+            this.foto.HeaderText = "Foto";
+            this.foto.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.foto.MinimumWidth = 6;
+            this.foto.Name = "foto";
+            this.foto.ReadOnly = true;
+            this.foto.Width = 50;
+            // 
+            // nome
+            // 
+            this.nome.DataPropertyName = "nome";
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.nome.DefaultCellStyle = dataGridViewCellStyle2;
+            this.nome.HeaderText = "Nome do Cliente";
+            this.nome.MinimumWidth = 6;
+            this.nome.Name = "nome";
+            this.nome.ReadOnly = true;
+            this.nome.Width = 304;
+            // 
+            // nasc
+            // 
+            this.nasc.DataPropertyName = "nasc";
+            dataGridViewCellStyle3.Format = "d";
+            dataGridViewCellStyle3.NullValue = null;
+            this.nasc.DefaultCellStyle = dataGridViewCellStyle3;
+            this.nasc.HeaderText = "Data de Nascimento";
+            this.nasc.MinimumWidth = 6;
+            this.nasc.Name = "nasc";
+            this.nasc.ReadOnly = true;
+            this.nasc.Width = 125;
+            // 
+            // documento
+            // 
+            this.documento.DataPropertyName = "documento";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.documento.DefaultCellStyle = dataGridViewCellStyle4;
+            this.documento.HeaderText = "Documento";
+            this.documento.MinimumWidth = 6;
+            this.documento.Name = "documento";
+            this.documento.ReadOnly = true;
+            this.documento.Width = 182;
+            // 
+            // celular
+            // 
+            this.celular.DataPropertyName = "celular";
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.celular.DefaultCellStyle = dataGridViewCellStyle5;
+            this.celular.HeaderText = "Celular";
+            this.celular.MinimumWidth = 6;
+            this.celular.Name = "celular";
+            this.celular.ReadOnly = true;
+            this.celular.Width = 182;
+            // 
+            // endereco
+            // 
+            this.endereco.DataPropertyName = "endereco";
+            this.endereco.HeaderText = "Endereço";
+            this.endereco.MinimumWidth = 6;
+            this.endereco.Name = "endereco";
+            this.endereco.ReadOnly = true;
+            this.endereco.Width = 244;
+            // 
+            // email
+            // 
+            this.email.DataPropertyName = "email";
+            dataGridViewCellStyle6.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.email.DefaultCellStyle = dataGridViewCellStyle6;
+            this.email.HeaderText = "E-mail";
+            this.email.MinimumWidth = 6;
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            this.email.Width = 193;
+            // 
+            // genero
+            // 
+            this.genero.DataPropertyName = "genero";
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.genero.DefaultCellStyle = dataGridViewCellStyle7;
+            this.genero.HeaderText = "Genero";
+            this.genero.MinimumWidth = 6;
+            this.genero.Name = "genero";
+            this.genero.ReadOnly = true;
+            this.genero.Width = 125;
+            // 
+            // rg
+            // 
+            this.rg.DataPropertyName = "rg";
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.rg.DefaultCellStyle = dataGridViewCellStyle8;
+            this.rg.HeaderText = "N° RG";
+            this.rg.MinimumWidth = 6;
+            this.rg.Name = "rg";
+            this.rg.ReadOnly = true;
+            this.rg.Width = 125;
+            // 
+            // estado_civil
+            // 
+            this.estado_civil.DataPropertyName = "estado_civil";
+            dataGridViewCellStyle9.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.estado_civil.DefaultCellStyle = dataGridViewCellStyle9;
+            this.estado_civil.HeaderText = "Estado Civil";
+            this.estado_civil.MinimumWidth = 6;
+            this.estado_civil.Name = "estado_civil";
+            this.estado_civil.ReadOnly = true;
+            this.estado_civil.Width = 141;
+            // 
+            // cep
+            // 
+            this.cep.DataPropertyName = "cep";
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.cep.DefaultCellStyle = dataGridViewCellStyle10;
+            this.cep.HeaderText = "Cep";
+            this.cep.MinimumWidth = 6;
+            this.cep.Name = "cep";
+            this.cep.ReadOnly = true;
+            this.cep.Width = 125;
+            // 
+            // numero
+            // 
+            this.numero.DataPropertyName = "numero";
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.numero.DefaultCellStyle = dataGridViewCellStyle11;
+            this.numero.HeaderText = "Número";
+            this.numero.MinimumWidth = 6;
+            this.numero.Name = "numero";
+            this.numero.ReadOnly = true;
+            this.numero.Width = 81;
+            // 
+            // bairro
+            // 
+            this.bairro.DataPropertyName = "bairro";
+            dataGridViewCellStyle12.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.bairro.DefaultCellStyle = dataGridViewCellStyle12;
+            this.bairro.HeaderText = "Bairro";
+            this.bairro.MinimumWidth = 6;
+            this.bairro.Name = "bairro";
+            this.bairro.ReadOnly = true;
+            this.bairro.Width = 278;
+            // 
+            // cidade
+            // 
+            this.cidade.DataPropertyName = "cidade";
+            dataGridViewCellStyle13.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.cidade.DefaultCellStyle = dataGridViewCellStyle13;
+            this.cidade.HeaderText = "Cidade";
+            this.cidade.MinimumWidth = 6;
+            this.cidade.Name = "cidade";
+            this.cidade.ReadOnly = true;
+            this.cidade.Width = 218;
+            // 
+            // obs
+            // 
+            this.obs.DataPropertyName = "obs";
+            this.obs.HeaderText = "Obs";
+            this.obs.MinimumWidth = 6;
+            this.obs.Name = "obs";
+            this.obs.ReadOnly = true;
+            this.obs.Width = 184;
+            // 
+            // situacao
+            // 
+            this.situacao.DataPropertyName = "situacao";
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.situacao.DefaultCellStyle = dataGridViewCellStyle14;
+            this.situacao.HeaderText = "Situação";
+            this.situacao.MinimumWidth = 6;
+            this.situacao.Name = "situacao";
+            this.situacao.ReadOnly = true;
+            this.situacao.Width = 125;
+            // 
+            // estado
+            // 
+            this.estado.DataPropertyName = "estado";
+            dataGridViewCellStyle15.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.estado.DefaultCellStyle = dataGridViewCellStyle15;
+            this.estado.HeaderText = "Estado";
+            this.estado.MinimumWidth = 6;
+            this.estado.Name = "estado";
+            this.estado.ReadOnly = true;
+            this.estado.Width = 147;
             // 
             // MenuClientes
             // 
@@ -123,7 +373,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::LaçosElô.Cadastro.Cliente.Properties.Resources.fundo_Azul;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(1518, 673);
             this.Controls.Add(this.dgLista);
             this.Controls.Add(this.groupBox1);
             this.DoubleBuffered = true;
@@ -150,5 +400,23 @@
         private System.Windows.Forms.Button btPdf;
         private System.Windows.Forms.Button btEditar;
         private System.Windows.Forms.DataGridView dgLista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewImageColumn foto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nasc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn documento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn celular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn endereco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estado_civil;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cep;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bairro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn obs;
+        private System.Windows.Forms.DataGridViewTextBoxColumn situacao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estado;
     }
 }
--- LaçosElô.Cadastro.Cliente/MenuClientes.cs
+++ LaçosElô.Cadastro.Cliente/MenuClientes.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace LaçosElô.Cadastro.Cliente {
         public MenuClientes() {
             InitializeComponent();
         }
-
+        string caminhoFotos = AppDomain.CurrentDomain.BaseDirectory + "Fotos/";
         private void MenuClientes_Load(object sender, EventArgs e) {
             // Abre maximizado
             this.WindowState = FormWindowState.Maximized;
@@ -25,8 +26,27 @@ namespace LaçosElô.Cadastro.Cliente {
             this.MaximizeBox = false;
             this.FormBorderStyle = FormBorderStyle.FixedSingle;
 
-           
+
             dgLista.DataSource = Funcoes.BuscaSQL("SELECT * FROM clientes");
+
+            foreach (DataGridViewRow lin in dgLista.Rows) {
+                if (lin.Cells["situacao"].Value != null) {
+                    string situacao = lin.Cells["situacao"].Value.ToString();
+
+                    if (situacao == "Cancelado") {
+                        lin.DefaultCellStyle.ForeColor = Color.Crimson;
+                    } else if (situacao == "Ativo") {
+                        lin.DefaultCellStyle.ForeColor = Color.Blue;
+                    }
+                }
+
+                if (File.Exists(caminhoFotos + lin.Cells["id"].Value.ToString() + ".png")) {
+                    lin.Cells["foto"].Value =
+                        Image.FromFile(caminhoFotos + lin.Cells["id"].Value.ToString() + ".png");
+                } else {
+                    lin.Cells["foto"].Value = Properties.Resources.avatar_icone;
+                }
+            }
         }
 
         private void btAdd_Click(object sender, EventArgs e) {
@@ -38,5 +58,11 @@ namespace LaçosElô.Cadastro.Cliente {
         private void groupBox1_Enter(object sender, EventArgs e) {
             
         }
+
+        private void dgLista_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+            
+        }
+
     }
 }
